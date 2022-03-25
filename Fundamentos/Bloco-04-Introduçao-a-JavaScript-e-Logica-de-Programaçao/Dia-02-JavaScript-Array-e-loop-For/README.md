9b683fb12309a56dee887720413ada2602a35640
Davi <davivarejao@gmail.com>
2022-03-25 00:55:23 -0300

    exercicio bloco-04-dia-02

---
+++ Fundamentos/Bloco-04-Introduçao-a-JavaScript-e-Logica-de-Programaçao/Dia-02-JavaScript-Array-e-loop-For/README.md
@@ -0,0 +1,3 @@
+# Array e loop For
+
+Uma série de exercícios anexada, manejando Arrays em conjunto com vários tipos de For e For/of para diferentes objetivos, como determinar o maior número da lista e fazer operações em todos os elementos de forma automática.
---
+++ Fundamentos/Bloco-04-Introduçao-a-JavaScript-e-Logica-de-Programaçao/Dia-02-JavaScript-Array-e-loop-For/exercise.js
@@ -0,0 +1,89 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+// Imprimir todos os itens do array
+
+
+for (let number of numbers) {
+  console.log(number);
+}
+
+// Somar todos os itens do array;
+
+let total = 0;
+
+for (let number of numbers) {
+  total = total + number;
+}
+
+console.log(total);
+
+// Média aritmética
+
+let media = 0;
+media = total / numbers.length;
+console.log(media);
+
+// Verificar se a média é maior que 20
+
+if (media > 20) {
+  console.log('A média é maior que 20');
+} else {
+  console.log('A média é menor que 20');
+}
+
+// Imprimir maior valor do array
+
+let bigNumber = 0;
+
+for (let number of numbers) {
+  if (number >= bigNumber) {
+    bigNumber = number;
+  }
+}
+
+console.log(bigNumber);
+
+// Imprimir menor valor
+
+let smallNumber = numbers[0];
+
+for (let number of numbers) {
+  if (number <= smallNumber) {
+    smallNumber = number;
+  }
+}
+
+console.log(smallNumber);
+
+// Contagem de quantos valores são ímpares
+
+let contagem = 0;
+
+for (const number of numbers) {
+  if ((number % 2) !== 0) {
+    contagem++;
+  }
+}
+
+if (contagem === 0) {
+  console.log('nenhum valor ímpar encontrado');
+} else {
+  console.log(contagem + ' valores ímpares encontrados');
+}
+
+// Criar um for que conta de 1 até 25 vezes
+
+for (let index = 0; index < 25; index++) {
+  console.log(index + 1);
+}
+
+// Imprimir o resultado de cada elemento de numbers dividido por 2
+
+const numbersDivision = [];
+
+for (const number of numbers) {
+  const division = number / 2;
+  numbersDivision.push(division);
+}
+
+console.log(numbersDivision);
