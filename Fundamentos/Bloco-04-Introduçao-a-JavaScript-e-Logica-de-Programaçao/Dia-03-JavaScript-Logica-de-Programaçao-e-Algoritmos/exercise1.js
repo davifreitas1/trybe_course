99af0fe13d7bc0ec2de4eead5b3f094140615316
Davi <davivarejao@gmail.com>
2022-03-28 23:13:02 -0300

    exercicio 1 bloco-04-dia-03

---
+++ Fundamentos/Bloco-04-Introduçao-a-JavaScript-e-Logica-de-Programaçao/Dia-03-JavaScript-Logica-de-Programaçao-e-Algoritmos/exercise1.js
@@ -0,0 +1,10 @@
+// Criar uma função que execute fatoração.
+
+const number = 5;
+let result = 1;
+
+for (let index = 1; index <= number; index++) {
+  result = result * index;
+}
+
+console.log(result);
